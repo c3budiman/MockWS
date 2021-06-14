470e6a366460aa0d097b856f0c7948bed72e1c35
Cecep Budiman <c3budiman@gmail.com>
2021-06-14 10:56:51 +0700

    first

---
+++ client.js
@@ -0,0 +1,22 @@
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://localhost:8080');
+
+ws.on('open', function open() {
+	var response = {
+		"message": 
+		"Update Versi Baru Dokumen. Versi terbaru Panduan PJJ final telah terupload"
+	}
+
+
+	ws.send('gela'+JSON.stringify(response), {},(tes) => {
+		console.log('callback');
+	});
+	ws.on('message', function incoming(message) {
+		console.log('received from server: %s', message);
+	});
+});
+
+// ws.on('message', function incoming(data) {
+// console.log(data);
+// });
---
+++ server.js
@@ -0,0 +1,26 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+
+    if(message.substring(0,4) == "gela") {
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message.substring(4));
+        }
+      });
+    }
+  });
+
+  var welcome = {
+    "kon" : "Welcome",
+}
+
+  ws.send(JSON.stringify(welcome));
+  // setInterval(()=> {
+  //   ws.send('tiap 3 detik');
+  // }, 3000);
+});
